6ec962c67da7066c7f1308e34b7abcc96d5e1fce
Ewan Brain <ewan.brain@gmail.com>
2021-02-12 16:04:14 +0000

    PlaneType enum set up

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,17 @@
 public enum PlaneType {
+
+    GOING_hUg3(200, 32000.0),
+    GOING_B1g(100, 16000.0),
+    GOING_W33(50, 8000.0);
+
+    private int capacity;
+    private double totalWeight;
+
+    PlaneType(int capacity, double totalWeight) {
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+
+
 }
