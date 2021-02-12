52b9c0a0eef4123761cbd70ba4cbaa71f61c0dca
Ewan Brain <ewan.brain@gmail.com>
2021-02-12 15:48:37 +0000

    pilot class created and tested

--- src/main/java/People/Crew/Pilot.java
+++ src/main/java/People/Crew/Pilot.java
@@ -1,4 +1,20 @@
 package People.Crew;
 
-public class Pilot {
+public class Pilot extends CabinCrewMember{
+
+    private String pilotLicenceNumber;
+
+    public Pilot(String name, RankType rank, String pilotLicenceNumber) {
+        super(name, rank);
+        this.pilotLicenceNumber = pilotLicenceNumber;
+    }
+
+
+    public String getLicenceNumber() {
+        return this.pilotLicenceNumber;
+    }
+
+    public String flyPlane(String string) {
+        return string;
+    }
 }
--- src/test/java/PilotTest.java
+++ src/test/java/PilotTest.java
@@ -1,2 +1,37 @@
+import People.Crew.Pilot;
+import People.Crew.RankType;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before() {
+        pilot = new Pilot ("Tia Kofi", RankType.CAPTAIN, "RPDRUKS2");
+
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Tia Kofi", pilot.getName());
+    }
+
+    @Test
+    public void hasRank() {
+        assertEquals(RankType.CAPTAIN, pilot.getRank());
+    }
+
+    @Test
+    public void hasLicenceNumber() {
+        assertEquals("RPDRUKS2", pilot.getLicenceNumber());
+    }
+
+    @Test
+    public void canFlyPlane() {
+        assertEquals("I picked a bad day to stop sniffing glue.", pilot.flyPlane("I picked a bad day to stop sniffing glue."));
+    }
 }
