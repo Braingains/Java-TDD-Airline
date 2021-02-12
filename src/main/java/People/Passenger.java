a281e617f52cd6bbd2444295d207557ed8d12fbf
Ewan Brain <ewan.brain@gmail.com>
2021-02-12 15:09:34 +0000

    passenger tests passing

--- src/main/java/People/Passenger.java
+++ src/main/java/People/Passenger.java
@@ -3,8 +3,14 @@ package People;
 public class Passenger extends Person {
 
 
+    private int numberOfBags;
+
     public Passenger(String name, int numberOfBags) {
         super(name);
         this.numberOfBags = numberOfBags;
+    }
+
+    public int getNumberOfBags() {
+        return this.numberOfBags;
     }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,25 @@
+import People.Passenger;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+    Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Lawrence Cheney", 1);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Lawrence Cheney", this.passenger.getName());
+    }
+
+    @Test
+    public void hasBags(){
+        assertEquals(1, this.passenger.getNumberOfBags());
+    }
+
 }
