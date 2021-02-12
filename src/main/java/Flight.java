79c583761b9daf6d32220d0d93ad5993068cb20a
Ewan Brain <ewan.brain@gmail.com>
2021-02-12 16:55:14 +0000

    setting up flightTest

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,31 @@
+import People.Crew.CabinCrewMember;
+import People.Crew.Pilot;
+import People.Passenger;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Pilot> pilots;
+    private ArrayList<CabinCrewMember> cabinCrewMembers;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destinationAirport;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(ArrayList<Pilot> pilots, ArrayList<CabinCrewMember> cabinCrewMembers, ArrayList<Passenger> passengers, Plane plane, String flightNumber, String destinationAirport, String departureAirport, String departureTime) {
+        this.pilots = pilots;
+        this.cabinCrewMembers = cabinCrewMembers;
+        this.passengers = passengers;
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destinationAirport = destinationAirport;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,35 @@
+import People.Crew.CabinCrewMember;
+import People.Crew.Pilot;
+import People.Crew.RankType;
+import People.Passenger;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
 public class FlightTest {
+
+    Pilot pilot1;
+    Pilot pilot2;
+    CabinCrewMember cabinCrewMember1;
+    CabinCrewMember cabinCrewMember2;
+    Passenger passenger1;
+    Passenger passenger2;
+    Plane plane1;
+    Plane plane2;
+    Flight EDItoYYZ;
+    Flight PSLtoPER;
+
+    @Before
+    public void before() {
+        pilot1 = new Pilot("Tayce", RankType.CAPTAIN, "RPDRUKS21");
+        pilot2 = new Pilot("A'Whorea", RankType.FIRST_OFFICER, "RPDRUKS22");
+        cabinCrewMember1 = new CabinCrewMember("Bimini Bon-Boulash", RankType.FLIGHT_ATTENDANT);
+        cabinCrewMember2 = new CabinCrewMember("Joe Black", RankType.FLIGHT_ATTENDANT);
+        passenger1 = new Passenger("Rupaul Charles", 4);
+        passenger2 = new Passenger("Michelle Visage", 67);
+        plane1 = new Plane (PlaneType.GOING_hUg3);
+        plane2 = new Plane (PlaneType.GOING_W33);
+        EDItoYYZ = new Flight(,2,100, plane1,"kW33N", "YYZ", "EDI", "10:30");
+
+    }
 }
